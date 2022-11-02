1a5ab1a7333c5768ac6a5cc9b8be1588afb17409
liamholland <114741709+liamholland@users.noreply.github.com>
2022-11-02 15:51:43 +0000

    Add files via upload

---
+++ Server/add_friend.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+id=$1
+friend=$2
+
+./checkIDs.sh $id $friend #check the files
+check=$?	#check what the exit code was
+
+if [ $check -eq 0 ]; then
+	grep $friend $id/friends.txt > /dev/null
+	exists=$?
+	if [ $exists -eq 1 ]; then
+		echo $friend >> $id/friends.txt
+		echo "ok: friend added"
+	else
+		echo "Friend already added"
+		exit 1
+	fi
+elif [ $check -eq 2 ]; then
+	echo "nok: user $id does not exist"
+elif [ $check -eq 3 ]; then
+	echo "nok: $friend does not exist"
+fi
+exit 0
---
+++ Server/createUser.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+user=$1
+./checkIDs.sh $user
+# running the command, then checking the exit code
+check=$?
+
+if [ $# -ne 1 ]; then
+        echo "nok: incorrect number of arguments"
+        exit 1
+elif [ $check -eq 0 ] ; then
+        echo "nok: user already exists"
+        exit 2
+elif [ $check -eq 2 ]; then
+        mkdir $user
+	touch $user/friends.txt
+	touch $user/wall.txt
+        echo "ok: user created"
+        exit 0
+fi
