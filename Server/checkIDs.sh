1833469b26c0d084ee6cc98a63c92f68d5c91589
redraincatching <99604494+redraincatching@users.noreply.github.com>
2022-11-02 15:01:26 +0000

    Update checkIDs.sh

--- Server/checkIDs.sh
+++ Server/checkIDs.sh
@@ -4,11 +4,14 @@ id1=$1
 id2=$2
 
 if [ $# -gt 2 ]; then
-	echo "too many arguments called on id check"
+	echo "nok: too many arguments called on id check"
 	exit 1	#exits with code 1 if there are too many arguments
 elif ! [ -d /id1 ]; then
+	echo "nok: id does not exist"
 	exit 2	#exits with code 2 if the first id does not exist
 elif [ $# -ne 1 ] && ! [ -d /id2 ]; then
-	exit 3	#exits with code 3 if the second id does not exits and there was a second argument passed
+	echo "nok: second user does not exist"
+	exit 3	#exits with code 3 if the second id does not exist and there was a second argument passed
 fi
+echo "ok: user(s) exist"
 exit 0
