23fbdf86d0b19ade9f7b6e321f905aa17a8f9158
redraincatching <99604494+redraincatching@users.noreply.github.com>
2022-11-03 17:34:11 +0000

    Update checkFriends.sh

--- Server/checkFriends.sh
+++ Server/checkFriends.sh
@@ -6,11 +6,13 @@ user2=$2
 check=$?
 
 if [ $check -ne 0 ];then
-	exit 1
+	exit 1	#first check that all users exist
 fi
 
 grep $user1 $user2/friends.txt > /dev/null
-exists=$?
+exists=$?	
+#search user2's friends for user1 
+#note: friendship is symmetrical
 if [ $exists -eq 1 ]; then
 	echo "nok: not friends"
 	exit 1
