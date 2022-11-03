5b9be6eb35bc16e780ae82453b7a2058f59c2c3e
redraincatching <99604494+redraincatching@users.noreply.github.com>
2022-11-03 17:42:39 +0000

    Update display_wall.sh
    just making sure it has exit codes and can handle the wrong input

--- Server/display_wall.sh
+++ Server/display_wall.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
 id=$1
+
+if [ $# -ne 1 ]; then
+	echo "nok: please only enter one user id"
+	exit 1
+fi
+
 ./checkIDs.sh $id
 check=$? #get the exit code from the user check
 
@@ -8,6 +14,8 @@ if [ $check -eq 0 ]; then
 	echo "start of file"
 	cat $id/wall.txt
 	echo "end of file"
+	exit 0
 elif [ $check -eq 2 ]; then
 	echo "nok: user $id does not exist"
+	exit 2
 fi
