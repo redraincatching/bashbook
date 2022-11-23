dea7783028fefa7dadc6f11e0b2a89cd65c2512e
redraincatching <redraincatching@disroot.org>
2022-11-23 16:36:59 +0000

    lock added, still broken

--- Server/server.sh
+++ Server/server.sh
@@ -1,42 +1,50 @@
 #! /bin/bash
+trap "rm Server/server" EXIT
 
 mkfifo Server/server
 
-echo "Reading from pipe" >Server/liam
-
 while true; do
-	read id request friendID other <Server/server #get a command
+	read id request friendID other < Server/server #get a command
 	case $request in #check what the command is and ensure validity for that command, then run the appropriate script
 		add)
 			if ! [ "$friendID" = '' ]; then 
 				./add_friend.sh "$id" "$friendID"
 				#check not null
+				echo "ok: friend $friendID added" 
 			else
-				echo "Specify the friend ID"
+				echo "nok: specify the friend ID"
 			fi
 			;;
 		post)
-			if ! { [ "$friendID" = '' ] || [ "$other" = '' ]; }; then
-				./post_messages.sh "$id" "$friendID" "$other"
-				#check if neither friend nor message are empty 
-			else
-				echo "Enter the friend ID followed by the message in quotes"
-			fi
+			while ! ln -s "$id" "$id"_ln 2>/dev/null
+				do
+					sleep 1
+				done
+
+				if ! { [ "$friendID" = '' ] || [ "$other" = '' ]; }; then
+					./post_messages.sh "$id" "$friendID" "$other"
+					#check if neither friend nor message are empty 
+					echo "ok: message posted"
+				else
+					echo "nok: enter the friend ID followed by the message in quotes"
+				fi
+
+			rm "$id"_ln
 			;;
 		display)
 			if ! [ "$friendID" = '' ]; then
 				./display_wall.sh "$friendID"
 			else
-				echo "Enter the friend ID"
+				echo "nok: enter the friend ID"
 			fi
 			;;
 		exit)
-			rm $id
+			rm Server/$id
 			echo "exited"
 			exit 0
 			;;
 		*)
-			echo "Only valid commands are add [friend ID], post [friend ID] [message], display [friend ID] or exit" > /dev/null
+			echo "only valid commands are add [friend ID], post [friend ID] [message], display [friend ID] or exit" > /dev/null
 			;;
 	esac
 done
--- client.sh
+++ client.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 id=$1
-
-trap "rm Server/$id" SIGINT #trap ctrl+c
+trap "rm Server/$id" EXIT #trap ctrl+c
 
 if [ $# -ne 1 ]; then
 	echo "error: this script requires one id as a parameter"
 	exit 1
 else
-	if ! [ -f Server/server ]; then
+	if ! [ -e Server/server ]; then
 		./Server/server.sh &
 		echo "started server"
 	fi
